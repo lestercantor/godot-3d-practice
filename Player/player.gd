281a1fba82c8409149aaea3ae4446acd2347bd4e
LesterCantor <c012718j@student.staffs.ac.uk>
2024-10-30 21:06:51 +0000

    changed how camera moves and rotates player instead. simple sprint

--- Player/player.gd
+++ Player/player.gd
@@ -1,18 +1,29 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-@export var sensitivity: float = 0.01
+
+var current_speed: float = 5.0
+
+# Different speeds for different states
+var walking_speed: float = 5.0
+var sprint_speed: float = walking_speed * 1.75
+var crouching_speed: float = walking_speed * 0.65
+
+
+@export var sensitivity: float = 0.3
 
 @onready var head: Node3D = $head
-@onready var camera: Camera3D = $head/Camera3D
 
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta: float) -> void:
+	
+	if Input.is_action_pressed("sprint"):
+		current_speed = sprint_speed
+	else:
+		current_speed = walking_speed
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -24,22 +35,23 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
 
 	move_and_slide()
+	print(current_speed)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		head.rotate_y(-event.relative.x * sensitivity)
-		camera.rotate_x(-event.relative.y * sensitivity)
-		camera.rotation.x = clampf(camera.rotation.x, deg_to_rad(-75), deg_to_rad(75))
+		rotate_y(deg_to_rad(-event.relative.x * sensitivity))
+		head.rotate_x(deg_to_rad(-event.relative.y * sensitivity))
+		head.rotation.x = clampf(head.rotation.x, deg_to_rad(-80), deg_to_rad(80))
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("exit"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_tree().quit()
