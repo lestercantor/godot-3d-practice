22874d4a52b038b6276ca582bf0d7e693cf36e85
LesterCantor <c012718j@student.staffs.ac.uk>
2024-12-11 20:12:20 +0000

    swap weapon refactor

--- Entity/Player/WeaponManager.gd
+++ Entity/Player/WeaponManager.gd
@@ -1,22 +1,25 @@
 extends Node3D
 class_name WeaponManager
 
-var primary_weapon: WeaponResource = null
+var active_weapon: WeaponResource = null
 var second_weapon: WeaponResource = null
 
-var primary_weapon_model: Node3D = null
+var active_weapon_model: Node3D = null
 var second_weapon_model: Node3D = null
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("swap_weapon"):
-		swap_weapon()
-		
+		helper_swap()
+	
+	if event.is_action_pressed("shoot"):
+		if active_weapon:
+			print(active_weapon.name)
 	# Debug purposes
 	if event.is_action_pressed("debug_key"):
-		if primary_weapon_model.visible == true:
-			print(primary_weapon.name)
-			print(primary_weapon.max_damage)
-		if second_weapon_model.visible == true:
+		if active_weapon_model:
+			print(active_weapon.name)
+			print(active_weapon.max_damage)
+		if second_weapon_model:
 			print(second_weapon.name)
 			print(second_weapon.max_damage)
 		
@@ -24,8 +27,8 @@ func _unhandled_input(event: InputEvent) -> void:
 # Used in the interaction of the world model weapon to assign the first weapon
 # that they pick up as their primary
 func equip_to_empty_slot(interacting_weapon: WeaponResource) -> bool:
-	if is_primary_weapon_empty():
-		assign_weapon("primary_weapon", interacting_weapon)
+	if is_active_weapon_empty():
+		assign_weapon("active_weapon", interacting_weapon)
 		return true
 	elif is_second_weapon_empty():
 		assign_weapon("second_weapon", interacting_weapon)
@@ -36,10 +39,10 @@ func equip_to_empty_slot(interacting_weapon: WeaponResource) -> bool:
 # Function to assign and show the weapon 
 func assign_weapon(weapon_ref: String, weapon: WeaponResource) -> void:
 	match weapon_ref:
-		"primary_weapon":
-			primary_weapon = weapon
-			primary_weapon_model = weapon.model.instantiate()
-			add_child(primary_weapon_model)
+		"active_weapon":
+			active_weapon = weapon
+			active_weapon_model = weapon.model.instantiate()
+			add_child(active_weapon_model)
 		"second_weapon":
 			second_weapon = weapon
 			second_weapon_model = weapon.model.instantiate()
@@ -51,38 +54,46 @@ func assign_weapon(weapon_ref: String, weapon: WeaponResource) -> void:
 
 func swap_weapon() -> void:
 	# return out of the function immediately if the player is holding nothing
-	if is_primary_weapon_empty(): 
+	if is_active_weapon_empty(): 
 		print("holding nothing")
 		return
 	
 	# Checks to make sure the player is holding 2 weapons before they can swap weapon
-	if (primary_weapon_model.visible == true) and !is_second_weapon_empty():
-		hide_weapon("primary_weapon")
-		show_weapon("second_weapon")
-		
-	elif !is_second_weapon_empty():
-		if second_weapon_model.visible == true:
-			hide_weapon("second_weapon")
-			show_weapon("primary_weapon")
-			
+	if active_weapon and !is_second_weapon_empty():
+		helper_swap()
+
 ################################################# Helper functions
-func is_primary_weapon_empty() -> bool: #########
-	return primary_weapon == null
+func is_active_weapon_empty() -> bool: #########
+	return active_weapon == null
 	
 func is_second_weapon_empty() -> bool: ##########
 	return second_weapon == null
 	
 func hide_weapon(weapon_ref: String) -> void: ###
 	match weapon_ref:
-		"primary_weapon":
-			primary_weapon_model.hide()
+		"active_weapon":
+			active_weapon_model.hide()
 		"second_weapon":
 			second_weapon_model.hide()
 
 func show_weapon(weapon_ref: String) -> void: ####
 	match weapon_ref:
-		"primary_weapon":
-			primary_weapon_model.show()
+		"active_weapon":
+			active_weapon_model.show()
 		"second_weapon":
 			second_weapon_model.show()
+			
+func helper_swap() -> void:
+	hide_weapon("active_weapon")
+	
+	var weapon_temp: WeaponResource = active_weapon
+	var weapon_model_temp: Node3D = active_weapon_model
+	
+	active_weapon = second_weapon
+	active_weapon_model = second_weapon_model
+	
+	second_weapon = weapon_temp
+	second_weapon_model = weapon_model_temp
+	
+	show_weapon("active_weapon")
 ################################################# Helper functions
